9ac8ba6e6f0a8ac5962ad4f54f6754da65bd173d
Chandan Kumar <chandan.kumar@global.loc>
2019-08-10 20:24:35 +0530

    Added build up script for the production server

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const server = express();
 const cors = require('cors');
 const fs = require('fs');
 const config = require('config');
+const path = require('path');
 
 //defining routes
 const users = require('./routes/api/v1/users');
@@ -31,9 +32,21 @@ server.use('/api/v1/auth',auth);
 server.use('/api/v1/assettypes',assettypes);
 server.use('/api/v1/assets',assets);
 
+
+//serve static assets if it's in production
+if(process.env.NODE_ENV === 'production'){
+	//set static folder
+	server.use(express.static('client/build'));
+	//checking the url host
+	// * means all are allowed
+	server.get('*',(req, res) => {
+		res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+	})
+
+}
 const port = process.env.PORT || 4000
 
 server.listen(port,() => console.log(`server is started at the ${port}`));
 // fs.open('index.txt','w',function(err,file){
 //     console.log(file);
-// })
+// })/^\+[0-9]{0,2}|0?\d{0,10}$/
